4913ce9daf23514bdf310a5dd990a7a1e12070aa
londumas <helion331990@gmail.com>
2019-01-25 10:29:10 -0700

    add CIV line

--- py/redvsblue/constants.py
+++ py/redvsblue/constants.py
@@ -1,5 +1,6 @@
 ### Absorber names and wavelengths [Angstrom]
 lines = {
     'MgII' : {'LINE':2796.3511, 'BLUE_MIN':2600., 'BLUE_MAX':2760., 'RED_MIN':2900. , 'RED_MAX':3120.},
+    'CIV'  : {'LINE':1548.2049, 'BLUE_MIN':1420., 'BLUE_MAX':1520., 'RED_MIN':1600.,  'RED_MAX':1700. },
     'LYA'  : {'LINE':1215.67,   'BLUE_MIN':1040., 'BLUE_MAX':1200., 'RED_MIN':1260.,  'RED_MAX':1375. },
 }
