a4f86e873b0ab5c58947bf9fd3005ebf6aedf394
londumas <helion331990@gmail.com>
2019-01-24 23:44:08 -0700

    able to read DR14Q also

--- py/redvsblue/read_SDSS_data.py
+++ py/redvsblue/read_SDSS_data.py
@@ -27,8 +27,8 @@ def cat_DR12Q(pathData,zmin,zmax,zkey='Z_VI'):
 
     h = fitsio.FITS(pathData)
     for k in ['PLATE','MJD','FIBERID','THING_ID']:
-        dic[k] = h['DR12Q_v2_10.fits'][k][:]
-    dic['Z'] = h['DR12Q_v2_10.fits'][zkey][:]
+        dic[k] = h[1][k][:]
+    dic['Z'] = h[1][zkey][:]
     h.close()
 
     w = (dic['Z']!=-1.) & (dic['Z']>zmin) & (dic['Z']<zmax)
@@ -101,7 +101,11 @@ def read_SDSS_data(path_DR12Q, path_spec, lines, zmin=0., zmax=10., zkey='Z_VI',
         m = tpm%100000
         w = pm==tpm
 
-        lam, fl, iv = p_read_spec_spplate(p,m)
+        try:
+            lam, fl, iv = p_read_spec_spplate(p,m)
+        except OSError:
+            print('WARNING: Can not find PLATE={}, MJD={}'.format(p,m))
+            continue
         print('{}: read {} objects from PLATE={}, MJD={}'.format(len(data.keys()),w.sum(),p,m))
 
         thids = catQSO['THING_ID'][w]
