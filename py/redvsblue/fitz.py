36da8a40dba79c85cc188c468a4465e389f3bff5
londumas <helion331990@gmail.com>
2019-01-28 18:15:35 -0700

    add finding precise minimum

---
+++ py/redvsblue/fitz.py
@@ -0,0 +1,37 @@
+import scipy as sp
+from redvsblue.zwarning import ZWarningMask as ZW
+
+def minfit(x, y):
+    """
+
+    """
+
+    if x.size<3:
+        return (-1,-1,-1,ZW.BAD_MINFIT)
+
+    try:
+        #- y = a x^2 + b x + c
+        a,b,c = sp.polyfit(x,y,2)
+    except sp.linalg.LinAlgError:
+        return (-1,-1,-1,ZW.BAD_MINFIT)
+
+    if a==0.:
+        return (-1,-1,-1,ZW.BAD_MINFIT)
+
+    #- recast as y = y0 + ((x-x0)/xerr)^2
+    x0 = -b/(2.*a)
+    y0 = -(b**2)/(4.*a)+c
+
+    zwarn = 0
+    if x0<=x.min() or x0>=x.max():
+        zwarn |= ZW.BAD_MINFIT
+    if y0<=0.:
+        zwarn |= ZW.BAD_MINFIT
+
+    if a>0.:
+        xerr = 1./sp.sqrt(a)
+    else:
+        xerr = 1./sp.sqrt(-a)
+        zwarn |= ZW.BAD_MINFIT
+
+    return x0, xerr, y0, zwarn
