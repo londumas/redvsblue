5742285e67efe9e1ce8317e473f8908d70d0a3f4
londumas <helion331990@gmail.com>
2019-01-29 21:33:36 -0700

    plot PCAs

--- py/redvsblue/utils.py
+++ py/redvsblue/utils.py
@@ -10,6 +10,7 @@ try:
 except ImportError:
     import builtins as __builtin__
 
+import redvsblue.constants
 
 def print(*args, **kwds):
     __builtin__.print(*args,**kwds)
@@ -91,8 +92,8 @@ def plot_flux_calibration(path):
     h.close()
 
     plt.plot(10**ll_st[w],st[w])
-    plt.xlabel(r'$\lambda_{\mathrm{Obs.}}\,[\mathrm{AA{}}]$')
-    plt.xlabel(r'$C(\lambda_{\mathrm{Obs.}})$')
+    plt.xlabel(r'$\lambda_{\mathrm{Obs.}}\,[\mathrm{\AA}]$')
+    plt.ylabel(r'$C(\lambda_{\mathrm{Obs.}})$')
     plt.grid()
     plt.show()
 
@@ -105,10 +106,38 @@ def plot_ivar_calibration(path):
     h.close()
 
     plt.plot(10**ll,eta)
-    plt.xlabel(r'$\lambda_{\mathrm{Obs.}}\,[\mathrm{AA{}}]$')
-    plt.xlabel(r'$\eta(\lambda_{\mathrm{Obs.}})$')
+    plt.xlabel(r'$\lambda_{\mathrm{Obs.}}\,[\mathrm{\AA}]$')
+    plt.ylabel(r'$\eta(\lambda_{\mathrm{Obs.}})$')
     plt.grid()
     plt.show()
+
+    return
+def plot_PCA(path,dwave_side=85):
+
+    h = fitsio.FITS(path)
+    head = h['BASIS_VECTORS'].read_header()
+    ll = sp.asarray(head['CRVAL1']+head['CDELT1']*sp.arange(head['NAXIS1']), dtype=sp.float64)
+    if 'LOGLAM' in head and head['LOGLAM']!=0:
+        ll = 10**ll
+    fl = sp.asarray(h['BASIS_VECTORS'].read(),dtype=sp.float64)
+    fl[0,:] /= fl[0,:].mean()
+    h.close()
+    qso_pca = [ interp1d(ll,fl[i,:],fill_value='extrapolate',kind='linear') for i in range(fl.shape[0]) ]
+
+    lines = redvsblue.constants.emissionLines
+
+    i = 0
+    for ln, lv in lines.items():
+        if ln=='PCA': continue
+        plt.plot(ll,fl[i,:],linewidth=2,color='black')
+        x = sp.linspace(lv-85,lv+85,2*85*10)
+        y = qso_pca[0](x)
+        plt.fill_between(x,y,alpha=0.8)
+        plt.plot([lv,lv],[0.,qso_pca[0](lv)],linewidth=2,alpha=0.8,color='red')
+        plt.xlabel(r'$\lambda_{\mathrm{Obs.}}\,[\mathrm{\AA}]$',fontsize=20)
+        plt.ylabel(r'$f_{\mathrm{PCA,'+str(i)+'}}$',fontsize=20)
+        plt.grid()
+        plt.show()
 
     return
 def read_mask_lines(path):
