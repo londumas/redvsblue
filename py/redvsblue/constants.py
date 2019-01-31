0e35b02cb8502baea99d73f2ebeec284fc613927
londumas <helion331990@gmail.com>
2019-01-31 10:53:24 -0700

    Line names in upper case

--- py/redvsblue/constants.py
+++ py/redvsblue/constants.py
@@ -1,13 +1,13 @@
 lines = {
-    'MgII' : {'LINE':2796.3511, 'BLUE_MIN':2600., 'BLUE_MAX':2760., 'RED_MIN':2900. , 'RED_MAX':3120.},
+    'MGII' : {'LINE':2796.3511, 'BLUE_MIN':2600., 'BLUE_MAX':2760., 'RED_MIN':2900. , 'RED_MAX':3120.},
     'CIV'  : {'LINE':1548.2049, 'BLUE_MIN':1420., 'BLUE_MAX':1520., 'RED_MIN':1600.,  'RED_MAX':1700. },
     'LYA'  : {'LINE':1215.67,   'BLUE_MIN':1040., 'BLUE_MAX':1200., 'RED_MIN':1260.,  'RED_MAX':1375. },
 }
 emissionLines = {
     'PCA'    : 0.,
-    'Halpha' : 6562.8,
-    'Hbeta'  : 4862.68,
-    'MgII'   : 2799.942,
+    'HALPHA' : 6562.8,
+    'HBETA'  : 4862.68,
+    'MGII'   : 2799.942,
     'CIII'   : 1908.734,
     'CIV'    : 1549.492,
     'LYA'    : 1215.67,
