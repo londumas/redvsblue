e1cf65c5eeed19d4cc3b1a97d7ad400405cabac8
londumas <helion331990@gmail.com>
2019-05-04 12:15:45 -0600

    Add file to test

--- py/redvsblue/test/test_cor.py
+++ py/redvsblue/test/test_cor.py
@@ -52,25 +52,24 @@ class TestCor(unittest.TestCase):
                 os.mkdir(self._branchFiles+fold)
 
         ###
-        urls = {'4300':'https://dr14.sdss.org/sas/dr14/eboss/spectro/redux/v5_10_0/spectra/lite/4300/spec-4300-55528-0224.fits',
-            '5138':'https://dr14.sdss.org/sas/dr14/eboss/spectro/redux/v5_10_0/spectra/lite/5138/spec-5138-55830-0020.fits'}
-        for n,p in urls.items():
-            print(self._branchFiles+'/data/spectra/lite/{}/{}'.format(n,p.split('/')[-1]))
-            if not os.path.isfile(self._branchFiles+'/data/spectra/lite/{}/{}'.format(n,p.split('/')[-1])):
-                wget.download(url=p,out=self._branchFiles+'/data/spectra/lite/{}/'.format(n))
+        path_to_data = 'https://dr14.sdss.org/sas/dr14/eboss/spectro/redux/v5_10_0/spectra/lite/'
+        dic = {'THING_ID':sp.array([113373895,242548124]),'PLATE':sp.array([4300,5138]),
+            'MJD':sp.array([55528,55830]),'FIBERID':sp.array([224,20]),'Z':sp.array([1.24624,2.87229])}
 
-        ###
-        plate = sp.array([4300,5138])
-        mjd = sp.array([55528,55830])
-        fid = sp.array([224,20])
-        thid = sp.array([0,1])
-        zqso = sp.array([1.24624,2.87229])
+        for i in range(len(dic['PLATE'])):
+            p = dic['PLATE'][i]
+            m = dic['MJD'][i]
+            f = dic['FIBERID'][i]
+            path = '{}/{}/spec-{}-{}-{}.fits'.format(path_to_data,str(p).zfill(4),str(p).zfill(4),m,str(f).zfill(4))
+            path2 = self._branchFiles+'/data/spectra/lite/{}/'.format(str(p).zfill(4))
+            path3 = path2+'/spec-{}-{}-{}.fits'.format(str(p).zfill(4),m,str(f).zfill(4))
+            print(path)
+            if not os.path.isfile(path3):
+                wget.download(url=path,out=path2)
 
         ### Save
         out = fitsio.FITS(self._branchFiles+'/Products/cat.fits','rw',clobber=True)
-        cols=[thid,plate,mjd,fid,zqso]
-        names=['THING_ID','PLATE','MJD','FIBERID','Z']
-        out.write(cols,names=names,extname='CAT')
+        out.write( [v for v in dic.values()] ,names=[k for k in dic.keys()],extname='CAT')
         out.close()
 
         return
@@ -81,6 +80,54 @@ class TestCor(unittest.TestCase):
 
         print("\n")
         shutil.rmtree(self._branchFiles, ignore_errors=True)
+
+        return
+    def compare_fits(self,path1,path2,nameRun=""):
+
+        print("\n")
+        m = fitsio.FITS(path1)
+        self.assertTrue(os.path.isfile(path2),"{}".format(nameRun))
+        b = fitsio.FITS(path2)
+
+        self.assertEqual(len(m),len(b),"{}".format(nameRun))
+
+        for i,_ in enumerate(m):
+
+            ###
+            r_m = m[i].read_header().records()
+            ld_m = []
+            for el in r_m:
+                name = el['name']
+                if len(name)>5 and name[:5]=="TTYPE":
+                    ld_m += [el['value'].replace(" ","")]
+            ###
+            r_b = b[i].read_header().records()
+            ld_b = []
+            for el in r_b:
+                name = el['name']
+                if len(name)>5 and name[:5]=="TTYPE":
+                    ld_b += [el['value'].replace(" ","")]
+
+            self.assertListEqual(ld_m,ld_b,"{}".format(nameRun))
+
+            for k in ld_m:
+                d_m = m[i][k][:]
+                d_b = b[i][k][:]
+                if d_m.dtype in ['<U23','S23']: ### for fitsio old version compatibility
+                    d_m = sp.char.strip(d_m)
+                if d_b.dtype in ['<U23','S23']: ### for fitsio old version compatibility
+                    d_b = sp.char.strip(d_b)
+                self.assertEqual(d_m.size,d_b.size,"{}: Header key is {}".format(nameRun,k))
+                if not sp.array_equal(d_m,d_b):
+                    print("WARNING: {}: Header key is {}, arrays are not exactly equal, using allclose".format(nameRun,k))
+                    diff = d_m-d_b
+                    w = d_m!=0.
+                    diff[w] = sp.absolute( diff[w]/d_m[w] )
+                    allclose = sp.allclose(d_m,d_b)
+                    self.assertTrue(allclose,"{}: Header key is {}, maximum relative difference is {}".format(nameRun,k,diff.max()))
+
+        m.close()
+        b.close()
 
         return
     def load_requirements(self):
@@ -113,6 +160,7 @@ class TestCor(unittest.TestCase):
         return
     def send_linefitter(self):
 
+        ###
         cmd = 'redvsblue_lineFitter.py --out '+self._branchFiles+'/Products/lineFitter.fits'
         cmd += ' --in-dir '+self._branchFiles+'/data/'
         cmd += ' --drq '+self._branchFiles+'/Products/cat.fits'
@@ -121,6 +169,13 @@ class TestCor(unittest.TestCase):
         cmd += ' --stack-obs'
         cmd += ' --no-extinction-correction'
         subprocess.call(cmd, shell=True)
+
+
+        ### Test
+        if self._test:
+            path1 = self._masterFiles + '/Products/lineFitter.fits'
+            path2 = self._branchFiles + '/Products/lineFitter.fits'
+            self.compare_fits(path1,path2,"redvsblue_lineFitter.py")
 
         return
 
